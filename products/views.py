a63b2434c86a9fc7a9fd704cd5a863bb78288a75
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-23 13:22:20 +0000

    Added search functionality
     Import Q to handle search queries to product view
    Update search form get request to filter by name or description
    Error message for empty terms added

--- products/views.py
+++ products/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render, get_object_or_404
-from.models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
 
 def view_products(request):
     """View to render the products page"""
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any details. Please check and try again")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
