3a1d69d9a7cc0a6326899f5eaac4a1c28b51ac78
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-25 11:37:17 +0000

    Fix syntax error
    Added commas missing from bag_contents context dictionary
    that was causing syntax error

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,15 +2,15 @@
 
 def bag_contents(request):
 
-    bag_items = {}
+    bag_items = []
     total = 0
     product_count = 0
 
 
     context = {
-        'bag_items': bag_items
-        'total': total
-        'product_count': product_count
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
     }
 
     return context
