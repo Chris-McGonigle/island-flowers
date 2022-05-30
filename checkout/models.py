4cf8c18ce2426238dda6e128438e351d133c118b
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-30 18:26:17 +0000

    Migrate checkout app
    Add string method to models in checkout app.
    Plan and migrate models

--- checkout/models.py
+++ checkout/models.py
@@ -41,6 +41,9 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
             super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     """Class to hold indiviudal order items"""
@@ -53,3 +56,14 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
                                          editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override default save method to set line item total
+        and update the grand total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
