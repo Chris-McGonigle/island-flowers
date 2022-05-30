5348714147890477a5e58c4b6fe217c54bad023b
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-30 18:08:28 +0000

    Add methods to checkout app classes
    Add method to generate random order number in Order class of checkout app.
    Add method to overwrite default save of order number has not been created.

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,21 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, blank=False, default=0)
+                                      
+    def _generate_order_number(self):
+        """Method to generate a unique order number"""
+        return uuid.uuid4().hex.upper()
+
+
+    def save(self, *args, **kwargs):
+        """
+        Override default save method to set order number
+        if it hasn't already been set
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+            super().save(*args, **kwargs)
+
 
 
 class OrderLineItem(models.Model):
