ff447244bbe0501da5a47daad5937131dda66fa4
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-31 12:39:31 +0000

    Install crispy forms
    Installed crispy forms to handle checkout form styling.
    Added to settings.py and requirments.txt files

--- island_flowers/settings.py
+++ island_flowers/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'island_flowers.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -63,6 +66,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
