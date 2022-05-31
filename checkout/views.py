6bea4cbbd47f0b8c71ed7e868b333889b76f2d21
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-31 16:59:43 +0000

    Add keys
    Add secret and public keys from stripe and export

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+import stripe
+
+from bag.contexts import bag_contents
 from .forms import OrderForm
 
 
@@ -12,6 +16,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "Your bag is empty.")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
