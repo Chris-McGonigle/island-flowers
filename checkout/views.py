047b8e5f46de224406f9dddd094200b1baa62ca7
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-31 16:18:45 +0000

    Add stripe payments
    Create stripe elements script and add to static files.
    CSS added to checkout.css from stripe documentation.
    CDN added to base.html

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L5WvdLVFJ15UZcywObrVqbMXMkpoE09q5MvDqZ8bUZGS1tC5Ngn6wYifeIMmWrmWwidM6Ald7gIIFYzff9cCeva00gmabeBiI',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
