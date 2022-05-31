79a202e842d0cd07d4b410de88e13bded77787e2
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-05-31 11:57:38 +0000

    Add delete update signal
    Create method to handle updating line item order total on delete.
    Import signals into apps.py to overqrite ready method.

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,12 +1,21 @@
 from django.db.models.signals import post_save, post_delete
-from django.dispath import receiver
+from django.dispatch import receiver
 
 from .models import OrderLineItem
+
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Method to update lineitem order total on newly
     created or updated lines
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def delete_on_save(sender, instance, **kwargs):
+    """
+    Method to update lineitem order total after line delete
     """
     instance.order.update_total()
