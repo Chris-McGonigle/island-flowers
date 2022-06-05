b6de988bfd9c63b1dbcd92272be3714ecc345474
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-06-05 12:19:57 +0000

    Initial blog set up
    Create model to handle user blog posts.
    Create view_blog view.
    Add blog html template.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Category
 
-# Create your models here.
+
+class Post(models.Model):
+    """Model to create blog posts"""
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=250)
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def view_blog(request):
+    """View to render main blog page"""
+
+    posts = Post.objects.filter().order_by("-created_on")[:10]
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts
+    }
+
+    return render(request, template, context )
