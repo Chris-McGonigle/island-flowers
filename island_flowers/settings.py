0311ca636777a4a87ee5c0a97c729b09621351c2
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-06-10 14:29:52 +0000

    Remove old secret key and replace with call to environ
    Debug set to development

--- island_flowers/settings.py
+++ island_flowers/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14e9njlgtqx)mi0-$7nf^@dqj4jb4-ymwndp6u)ith#%(ebc-n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['island-flowers.herokuapp.com', 'localhost']
 
