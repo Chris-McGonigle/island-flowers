3c0a5eb84544a3aff7ffed1507a21959d243bcd4
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-06-10 13:11:20 +0000

    Amend newsletter label input and changed input to not required.

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Subscriber
+from crispy_forms.helper import FormHelper
 
 
 class SubscriberForm(forms.ModelForm):
 
     sub_form = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.fields['email'].label = False
 
     class Meta:
         model = Subscriber
--- newsletter/models.py
+++ newsletter/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Subscriber(models.Model):
     """Class to hold subscriber details"""
 
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
