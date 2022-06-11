27ae54967d83a02847df7785f24e53cf31fdca83
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-06-11 11:42:24 +0000

    Add form helper to blog comments form to set defualt input field height

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Post, Comment
+from crispy_forms.helper import FormHelper
 
 
 class PostForm(ModelForm):
@@ -14,6 +15,13 @@ class PostForm(ModelForm):
 
 class CommentForm(ModelForm):
     """Form to handle user comments"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.fields['body'].label = False
+        self.helper.layout = Layout(
+            FIELD('body', rows='10'),
+            )
 
     class Meta:
         model = Comment
