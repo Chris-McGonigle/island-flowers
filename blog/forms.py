5421a265eb8602105d885a3e0124c8babea44c47
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-06-13 20:04:16 +0000

    Remove label from blog comment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Post, Comment
+from crispy_forms.helper import FormHelper
 
 
 class PostForm(ModelForm):
@@ -14,6 +15,10 @@ class PostForm(ModelForm):
 
 class CommentForm(ModelForm):
     """Form to handle user comments"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.fields['body'].label = False
 
     class Meta:
         model = Comment
